202008fdbdf0f364b8cf0fae1f2910849ecf17f7
IainCarlisle118 <Iain_Carlisle@hotmail.co.uk>
2018-03-19 08:28:10 +0000

    Implementation of article page

---
+++ NewsSite/Controllers/ArticleController.cs
@@ -0,0 +1,24 @@
+﻿using NewsSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewsSite.Controllers
+{
+    public class ArticleController : Controller
+    {
+        public Article article;
+        // GET: Article
+        public ActionResult Index(string urlToImg, string title, string description)
+        {
+            article = new Article();
+            article.urlToImg = urlToImg;
+            article.title = title;
+            article.description = description;
+
+            return View(article);
+        }
+    }
+}
