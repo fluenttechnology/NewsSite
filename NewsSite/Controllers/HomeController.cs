2bb991c8702f57a2c44208420b64988bfbd05102
IainCarlisle118 <Iain_Carlisle@hotmail.co.uk>
2018-03-19 08:52:22 +0000

    Conversion of API call to helper

--- NewsSite/Controllers/HomeController.cs
+++ NewsSite/Controllers/HomeController.cs
@@ -17,56 +17,23 @@ namespace NewsSite.Controllers
 
         public ActionResult Index(string searchBar, string pageNumber)
         {
-            if(pageNumber == null)
-            {
-                pageNumber = "1";
-            }
-            if (searchBar == null)
-            {
-                searchBar = "";
-            }
-
-            var url = "https://newsapi.org/v2/everything?" +
-                "domains=bbc.co.uk&" +
-                "pageSize=15&" +
-                "page=" + pageNumber + "&" +
-                "q=" + searchBar + "&" +
-                "apiKey=dbca6d85dbbd4e11b532b212af6282b5";
-
-            var json = new WebClient().DownloadString(url);
-
-            Articles = new Articles();
-            Articles.articles = new List<Article>();
-
-            JObject jObject = JObject.Parse(json);
-            JToken jArticles = jObject["articles"];
-            JToken jSource;
-
-            int i = 1;
-
-            foreach (var article in jArticles)
-            {
-                Article newArticle = new Article();
-
-                jSource = article["source"];
-
-                newArticle.id = i.ToString();
-                newArticle.source = (string)(jSource["id"]);
-                newArticle.name = (string)(jSource["name"]);
-                newArticle.author = (string)article["author"];
-                newArticle.title = (string)article["title"];
-                newArticle.description = (string)article["description"];
-                newArticle.url = (string)article["url"];
-                newArticle.urlToImg = (string)article["urlToImage"];
-                newArticle.publishedDate = (string)article["publishedAt"];
-
-                Articles.articles.Add(newArticle);
-            }
+            var query = "";
+            GetAPIResponse(query, searchBar, pageNumber);
 
             return View(Articles);
         }
         
         public PartialViewResult GetArticles(string searchBar, string pageNumber)
+        {
+            var query = "";
+            GetAPIResponse(query, searchBar, pageNumber);
+
+            return PartialView("_ArticlesView", Articles);
+        }
+
+        #region Helper functions
+
+        private Articles GetAPIResponse(string query, string searchBar, string pageNumber)
         {
             if (pageNumber == null)
             {
@@ -76,12 +43,13 @@ namespace NewsSite.Controllers
             {
                 searchBar = "";
             }
-           
+
             var url = "https://newsapi.org/v2/everything?" +
                 "domains=bbc.co.uk&" +
                 "pageSize=15&" +
                 "page=" + pageNumber + "&" +
                 "q=" + searchBar + "&" +
+                query +
                 "apiKey=dbca6d85dbbd4e11b532b212af6282b5";
 
             var json = new WebClient().DownloadString(url);
@@ -114,22 +82,9 @@ namespace NewsSite.Controllers
                 Articles.articles.Add(newArticle);
             }
 
-
-            return PartialView("_ArticlesView", Articles);
+            return Articles;
         }
 
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Article article = Articles.articles.Find(x => x.id == id.ToString());
-            if (article == null)
-            {
-                return HttpNotFound();
-            }
-            return View(article);
-        }        
+        #endregion
     }
 }
